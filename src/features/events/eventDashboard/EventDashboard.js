3f96fafafd3b86e08b1128839b03d7be830c6af7
dhillon64 <67638060+dhillon64@users.noreply.github.com>
2020-09-21 18:05:08 +0100

    added crud operations to app, can now add,edit and delete events from home page

--- src/features/events/eventDashboard/EventDashboard.js
+++ src/features/events/eventDashboard/EventDashboard.js
@@ -4,16 +4,51 @@ import EventList from "./EventList";
 import EventForm from "../eventForm/EventForm";
 import { sampleData } from "../../../app/api/sampleData";
 
-const EventDashboard = ({ formOpen, setFormOpen }) => {
+const EventDashboard = ({
+  formOpen,
+  setFormOpen,
+  selectEvent,
+  selectedEvent,
+}) => {
   const [events, setEvents] = useState(sampleData);
+
+  function handleDeleteEvent(eventId) {
+    setEvents(events.filter((event) => event.id !== eventId));
+  }
+
+  function handleCreateEvent(event) {
+    setEvents([...events, event]);
+  }
+
+  const handleUpdateEvent = (updatedEvent) => {
+    setEvents(
+      events.map((event) =>
+        event.id === updatedEvent.id ? updatedEvent : event
+      )
+    );
+    selectEvent(null);
+  };
 
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList events={events} />
+        <EventList
+          selectEvent={selectEvent}
+          events={events}
+          deleteEvent={handleDeleteEvent}
+        />
       </Grid.Column>
       <Grid.Column width={6}>
-        {formOpen && <EventForm setFormOpen={setFormOpen} />}
+        {formOpen && (
+          <EventForm
+            setFormOpen={setFormOpen}
+            setEvents={setEvents}
+            createEvent={handleCreateEvent}
+            selectedEvent={selectedEvent}
+            key={selectedEvent ? selectedEvent.id : null}
+            updateEvent={handleUpdateEvent}
+          />
+        )}
       </Grid.Column>
     </Grid>
   );
--- src/features/events/eventDashboard/EventList.js
+++ src/features/events/eventDashboard/EventList.js
@@ -2,12 +2,17 @@ import React, { Fragment } from "react";
 import EventListItem from "./EventListItem";
 
 const EventList = (props) => {
-  const { events } = props;
+  const { events, selectEvent, deleteEvent } = props;
 
   return (
     <Fragment>
       {events.map((event) => (
-        <EventListItem key={event.id} event={event} />
+        <EventListItem
+          key={event.id}
+          event={event}
+          selectEvent={selectEvent}
+          deleteEvent={deleteEvent}
+        />
       ))}
     </Fragment>
   );
